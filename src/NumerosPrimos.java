df6e27afaccffa807bb8eb807fe0766776ae9d16
hicham <hicham@PC02.mshome.net>
2017-05-17 12:22:14 +0200

    Archivo editado por hicham

---
+++ src/NumerosPrimos.java
@@ -0,0 +1,24 @@
+ // @author hicham
+
+public class NumerosPrimos {
+
+    public static void main(String[] args) {
+        int[] array = {1, 7, 8, 35, 22, 6, 2};
+        int n, i, cnp = 0, res;
+        
+        for (i = 0; i < 7; i++) {
+            res = 0;
+            for (int j = 1; j <= array[i]; j++) {
+                if (array[i] % j == 0) {
+                    res++;
+                }
+            }
+        if (res == 2) {
+                cnp++;
+                System.out.println("numero primo =" + array[i]);
+            }    
+        }
+        System.out.println("La cantidad de numeros primos es :" + cnp);
+    }
+
+}
